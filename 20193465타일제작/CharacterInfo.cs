cec113395d3bce9b0cbd49f3f2521a76d9f21705
Kyungmin Seo <71204429+kyungminei@users.noreply.github.com>
2022-05-04 02:08:53 +0900

    Add files via upload

--- 20193465타일제작/CharacterInfo.cs
+++ 20193465타일제작/CharacterInfo.cs
@@ -8,27 +8,39 @@ public class CharacterInfo : MonoBehaviour
 
     private GameObject clicked_tile; //클릭한 타일을 저장하는 함수
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="GoVisitedTileItem")
+        {
+            Debug.Log("아이템에 충돌했습니다");
+            IsGet_GovisitedTile_item = true;
+            Destroy(other.gameObject);
+        }
+    }
     void Update()
     {
+       
         //if문 중첩이 많아서 후에 수정해보기.
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+        //
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //    RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit))
-            {
-                if(hit.transform.gameObject.tag=="Tile")
-                {
-                    clicked_tile = hit.transform.gameObject;
-                }
-            }
-            //아이템을 먹은 상태이면서, 클릭한 타일이 한 번 방문했던 타일이라면
-            if(IsGet_GovisitedTile_item==true && clicked_tile.GetComponent<Tile>().IsVisited==true)
-            {
-                gameObject.transform.position = clicked_tile.transform.position; //클릭한 타일로 가기.
-                IsGet_GovisitedTile_item = false;
-            }
-        }
+        //    if (Physics.Raycast(ray, out hit))
+        //    {
+        //        if(hit.transform.gameObject.tag=="Tile")
+        //        {
+        //            Debug.Log("클릭한 타일 저장");
+        //            clicked_tile = hit.transform.gameObject;
+        //        }
+        //    }
+        //    //아이템을 먹은 상태이면서, 클릭한 타일이 한 번 방문했던 타일이라면
+        //    if(IsGet_GovisitedTile_item==true && clicked_tile.GetComponent<Tile>().IsVisited==true)
+        //    {
+        //        gameObject.transform.position = clicked_tile.transform.position; //클릭한 타일로 가기.
+        //        IsGet_GovisitedTile_item = false;
+        //    }
+        //}
     }
 }
--- 20193465타일제작/Tile.cs
+++ 20193465타일제작/Tile.cs
@@ -7,17 +7,14 @@ public class Tile : MonoBehaviour
     BoxCollider BoxCollide;
     new Renderer renderer;
 
-    public float Tilexsize = 0;
-    public float Tilezsize = 0;
+    public float Tilexsize = 3;
+    public float Tilezsize = 3;
     public bool IsVisited = false;
-
-    private GameObject player;  //프리팹의 인스펙터 창에 오브젝트 객체를 할당할 수 없음
 
     void Start()
     {
         renderer = gameObject.GetComponent<Renderer>();
         BoxCollide = GetComponent<BoxCollider>();
-        player = GameObject.FindGameObjectWithTag("Player");
 
         Tilexsize = BoxCollide.bounds.size.x;
         Tilezsize = BoxCollide.bounds.size.z;
@@ -28,6 +25,12 @@ public class Tile : MonoBehaviour
         if(IsVisited==true)
         {
             Debug.Log("이 타일은 왔었던 타일 입니다.");
+            //플레이어가 GoVIsitedTile 아이템을 먹은 상태라면
+            if(GameManager.Player1.GetComponent<CharacterInfo>().IsGet_GovisitedTile_item==true)
+            {
+                SetPlayerPosition();
+                GameManager.Player1.GetComponent<CharacterInfo>().IsGet_GovisitedTile_item = false;
+            }
         }
         else if(IsVisited==false)
         {
@@ -37,8 +40,19 @@ public class Tile : MonoBehaviour
 
     void SetPlayerPosition()
     {
-        player.transform.position = gameObject.transform.position;
+        GameManager.Player1.transform.position = gameObject.transform.position;
         IsVisited = true;
         renderer.material.color = Color.red;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag=="Tile")
+        {
+            for(int i=0;i<collision.contacts.Length;i++)
+            {
+                Debug.Log(collision.contacts[i].normal); //디버깅
+            }
+        }
     }
 }
